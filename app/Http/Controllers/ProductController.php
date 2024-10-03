f72c5d4f9ea98bc9220235ba52a7069203d5072f
unknown <admin@NB-BAK-QA-01.primacom.co.id>
2024-10-03 10:12:45 +0700

    Update L5 swagger

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -10,6 +10,33 @@ use function PHPUnit\Framework\isNull;
 
 class ProductController extends Controller
 {
+    /**
+     * @OA\Get(
+     *      path="/api/products",
+     *      operationId="index",
+     *      tags={"Product"},
+     *      summary="Get all products",
+     *      description="Get all products",
+     *      @OA\Response(
+     *          response=200,
+     *          description="Success",
+     *          @OA\JsonContent(
+     *              @OA\Property(property="status", type="integer", example=200),
+     *              @OA\Property(property="message", type="string", example="Get All products"),
+     *              @OA\Property(property="data", type="array",
+     *              @OA\Items(
+     *              @OA\Property(property="id", type="integer", example=1),
+     *              @OA\Property(property="name", type="string", example="Product 1"),
+     *              @OA\Property(property="price", type="integer", example=10000),
+     *              @OA\Property(property="description", type="string", example="Description product 1"),
+     *              @OA\Property(property="image", type="string", example="image.jpg"),
+     *              )
+     *            )
+     *        )
+     *     ),
+     * )
+     */
+
     public function index()
     {
         // $products = Product::all();
@@ -24,6 +51,30 @@ class ProductController extends Controller
         // ];
         return response()->json($products, Response::HTTP_OK);
     }
+
+
+    /**
+     * @OA\Get(
+     *      path="/api/products/{id}",
+     *      operationId="show",
+     *      tags={"Product Sendiri"},
+     *      summary="Get product by id",
+     *      description="Get product by id",
+     *      @OA\Parameter(
+     *          name="id",
+     *          description="Product id",
+     *          required=true,
+     *          in="path",
+     *          @OA\Schema(
+     *              type="integer"
+     *          )
+     *      ),
+     *      @OA\Response(response=200,description="Success",
+     *          @OA\JsonContent()
+     *      ),
+     *      @OA\Response(response=404, description="Resource Not Found"),
+     * )
+     */
 
     // Detail Product
     public function show($id)
@@ -44,6 +95,37 @@ class ProductController extends Controller
         ];
         return response()->json($data, Response::HTTP_OK);
     }
+
+    /**
+     * @OA\Post(
+     *      path="/api/products",
+     *      operationId="store",
+     *      tags={"Product"},
+     *      summary="Create new product",
+     *      description="Create new product",
+     *      @OA\RequestBody(
+     *          required=true,
+     *          @OA\MediaType(
+     *             mediaType="multipart/form-data",
+     *             @OA\Schema(
+     *                type="object",
+     *                required={"name", "price", "description", "image"},
+     *                @OA\Property(property="name", type="string", example="Product 1"),
+     *                @OA\Property(property="price", type="integer", example="10000"),
+     *                @OA\Property(property="description", type="string", example="Description product 1"),
+     *                @OA\Property(property="image", type="string", format="binary"),
+     *             ),
+     *         ),
+     *     ),
+     *     @OA\Response(
+     *         response=201,
+     *         description="Product created successfully",
+     *         @OA\JsonContent()
+     *     ),
+     *     @OA\Response(response=400, description="Bad request"),
+     *     @OA\Response(response=404, description="Resource Not Found"),
+     * )
+     */
 
     // Create Product
 
